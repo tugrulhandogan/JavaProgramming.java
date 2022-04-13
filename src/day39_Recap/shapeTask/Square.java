e85d256851c584838c1c6eea56edf40da7da279b
tugrulhandogan <97797613+tugrulhandogan@users.noreply.github.com>
2022-04-13 23:30:14 +0200

    day 39: Recap

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -2,8 +2,25 @@ package day39_Recap.shapeTask;
 
 public class Square extends Shape{
 
-    public Square(String name) {
-        super(name);
+    private double side;
+
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side <= 0){
+            System.err.println("Invalid side: " + side);
+            System.exit(1);
+        }
+
+        this.side = side;
+    }
+
+    public Square(String name, double side) {
+        super("Square");
+        setSide(side);
     }
 }
 /*
