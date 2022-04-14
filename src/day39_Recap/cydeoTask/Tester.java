e7c5a22835fcbe1c01cf85eb2468e579ee1164e9
tugrulhandogan <97797613+tugrulhandogan@users.noreply.github.com>
2022-04-14 16:16:35 +0200

    day 39: Recap

---
+++ src/day39_Recap/cydeoTask/Tester.java
@@ -0,0 +1,28 @@
+package day39_Recap.cydeoTask;
+
+public class Tester extends Employee{
+
+    public Tester(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+
+    public void createTicket(){
+        System.out.println(getJobTitle() + " " + getName() + " is creating ticket");
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle() + " " + getName() + " is testing the application ");
+    }
+
+
+
+}
+/*
+  3. Create a sub class of Employee named Tester
+
+            Override the work method
+
+            Extra methods:
+                createTicket()
+ */
